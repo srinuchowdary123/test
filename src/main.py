a75522a88d7c04fa47a95b12e8df3f5fbdeba7cc
srinu <srinu@gmail.com>
2024-11-16 16:33:28 +0000

    working on the threeway merge

--- src/main.py
+++ src/main.py
@@ -4,4 +4,7 @@ thi is the source code of my application
 this is second line of my code
 i am from the prod branch
 
+
+this line i want to remove while merging
+
 i have done the reuired changes in the code
