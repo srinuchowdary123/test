637fecf3cb421f93b0165d312b7c835b0eb2c381
srinu <srinu@gmail.com>
2024-11-16 16:34:56 +0000

    added the 3-way merge file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 thi is the source code of my application
 
 
+
 this is second line of my code
 i am from the prod branch
-
+ this line i has to be in the main branch only
 i have done the reuired changes in the code
